80cd9cc510a4452617aafb3e0ec552ab49950c60
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2023-01-14 13:48:32 -0300

    cors stuff

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,14 @@ builder.Services.AddDbContext<DataContext>(opts=>
 {
     opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt => 
+{
+    opt.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+        // policy.AllowAnyMethod().AllowAnyHeader()
+    });
+});
 
 var app = builder.Build();
 
