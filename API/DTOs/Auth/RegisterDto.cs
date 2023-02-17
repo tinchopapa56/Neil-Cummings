e16989e21a4fadfbb82de82c144cd112087230fa
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2023-02-17 01:33:14 -0300

    password validation

--- API/DTOs/Auth/RegisterDto.cs
+++ API/DTOs/Auth/RegisterDto.cs
@@ -14,8 +14,8 @@ namespace API.DTOs.Auth
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Pass must be complex")]
-        //
+        // [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Pass must be complex")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Pass must be complex")]
         public string Password { get; set; }
         [Required]
         public string Username { get; set; }
